631f15351f1941b5caabe354aed4cb313caf69ba
Murttaz <vincentfournier1993@gmail.com>
2022-11-03 17:27:32 +0100

    début Junit

---
+++ src/test/java/fr/diginamic/enumerations/SaisonTest.java
@@ -0,0 +1,14 @@
+package fr.diginamic.enumerations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SaisonTest {
+
+	@Test
+	public void testValueOfLibelle() {
+		
+	}
+
+}
